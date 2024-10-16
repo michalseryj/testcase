c85d0fac64ecf8a0a7a0ee31693d83837043b668
Michal Seryj <mseryj@veracode.com>
2024-10-16 17:48:57 +0200

    timeout update

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://datart.cz',
+    defaultCommandTimeout: 10000, // Set timeout to 10 seconds (10000 ms)
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
